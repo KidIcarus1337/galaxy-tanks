c04839024b471f33d3ab415725039c1035619b9e
KidIcarus1337 <kid_icarus3@yahoo.com>
2015-09-01 10:59:52 -0500

    Added full height/width canvas.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,39 @@
 $(function() {
 
     // Game mode/state change ----------------------------------------------------------------------------------------------------------
-    function modeChange(modeTear, modeBuild) {
-        $("body").off();
-        modeTear();
-        modeBuild();
+    function modeChange(tearMode, buildMode) {
+        $(window, "*").off();
+        tearMode();
+        buildMode();
     }
 
     function buildTitle() {
-        $(".title-screen").delay(500).fadeIn(500);
-        startLoop();
-        $(window).on("keydown click", titlePress);
+        $(".title-screen").delay(500).fadeIn(500, function() {
+            $(window).on("keydown click", titlePress);
+        });
+        stop_loop = false;
+        titleTextLoop();
     }
 
     function tearTitle() {
         $(".title-screen").fadeOut(500);
-        start_text.stop();
+        stop_loop = true;
+        title_text.stop(true, true);
     }
 
     function buildMenu() {
-        $(".menu-screen").delay(500).fadeIn(500);
+        $(".menu-screen").delay(500).fadeIn(500, function() {
+            $(window).keyup(function(e) {
+                if (e.keyCode == 27) {
+                    modeChange(tearMenu, buildTitle);
+                } else if (e.keyCode == 13) {
+                    modeChange(tearMenu, buildGame);
+                }
+            });
+            $(".menu-button").click(function() {
+                modeChange(tearMenu, buildGame);
+            });
+        });
     }
 
     function tearMenu() {
@@ -27,7 +41,9 @@ $(function() {
     }
 
     function buildGame() {
-        $(".game-screen").delay(500).fadeIn(500);
+        $(".game-screen").delay(500).fadeIn(500, function() {
+
+        });
     }
 
     function tearGame() {
@@ -39,11 +55,44 @@ $(function() {
         modeChange(tearTitle, buildMenu);
     }
 
-    var start_text = $(".title-container p");
-    function startLoop() {
-        start_text.animate({opacity:'-=0.7'}, 900);
-        start_text.animate({opacity:'+=1'}, 900, startLoop);
+    var stop_loop;
+    var title_text = $(".title-container p");
+    function titleTextLoop() {
+        title_text.animate({opacity:'0.25'}, 900, function() {
+            title_text.animate({opacity:'1'}, 900, function() {
+                if (stop_loop !== true) {
+                    titleTextLoop();
+                }
+            });
+        });
     }
     buildTitle();
 
+    // Menu functions ----------------------------------------------------------------------------------------------------------
+    function menuPress() {
+        modeChange(tearMenu, buildGame);
+    }
+
+    // Canvas functions ----------------------------------------------------------------------------------------------------------
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d');
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        /**
+         * Your drawings need to be inside this function otherwise they will be reset when
+         * you resize the browser window and the canvas goes will be cleared.
+         */
+        drawStuff();
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+        // do your drawing stuff here
+    }
 });
