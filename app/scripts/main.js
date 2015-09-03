5bfdfd22ed6acddbd0542a593366b08d25b4c92c
KidIcarus1337 <kid_icarus3@yahoo.com>
2015-09-03 16:09:13 -0500

    Added not-yet-functioning, adjustable aim and power parameters for firing shots.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -49,7 +49,7 @@ $(function() {
         $(".game-screen").fadeOut(500);
     }
 
-    // Title functions ----------------------------------------------------------------------------------------------------------
+    // Title ----------------------------------------------------------------------------------------------------------
     function titlePress() {
         modeChange(tearTitle, buildMenu);
     }
@@ -66,12 +66,12 @@ $(function() {
         });
     }
 
-    // Menu functions ----------------------------------------------------------------------------------------------------------
+    // Menu ----------------------------------------------------------------------------------------------------------
     function menuPress() {
         modeChange(tearMenu, buildGame);
     }
 
-    // Canvas functions ----------------------------------------------------------------------------------------------------------
+    // Canvas ----------------------------------------------------------------------------------------------------------
     var canvas = new fabric.Canvas('canvas');
 
     // resize the canvas to fill browser window dynamically
@@ -109,9 +109,126 @@ $(function() {
 
     canvas.add(ball);
 
-    canvas.on('mouse:down', function(options) {
+    $(".fire-button").on('click', function() {
         draw(0, 0);
+        $(".fire-button").off();
     });
 
-    buildTitle();
+    // Game UI ----------------------------------------------------------------------------------------------------------
+    function paramHover() {
+        $(".param-button").hover(function() {
+            $(this).stop().animate({backgroundColor: "#414141"}, 150)
+        }, function() {
+            $(this).stop().animate({backgroundColor: "#1f1f1f"}, 150)
+        });
+    }
+
+    var aimKeyPress;
+    var aimWheel;
+
+    function setAim() {
+        $(".aim").stop().animate({"font-size": 30}, 200);
+        $(".aim-button").animate({"width": 100, backgroundColor: "#414141"}, 200).off();
+        setTimeout(function() {
+            $(window).on("click", confirmAim);
+        }, 50);
+        var keyPressed = false;
+        aimKeyPress = function(e) {
+            key = e.keyCode || e.charCode;
+            var aim;
+            if ((key >= 48 || key >= 48) && (key <= 57 || key <= 48)) {
+                if (keyPressed == false) {
+                    aim = 0;
+                    keyPressed = true;
+                } else {
+                    aim = Number($(".aim").text().substring(0, $(".aim").text().length - 1));
+                }
+                if (((aim * 10) + (key - 48)) <= 360) {
+                    $(".aim").text((aim * 10) + (key - 48) + "˚");
+                }
+            } else if (key == 38) {
+                aim = Number($(".aim").text().substring(0, $(".aim").text().length - 1));
+                $(".aim").text(aim + 1  + "˚");
+            } else if (key == 40) {
+                aim = Number($(".aim").text().substring(0, $(".aim").text().length - 1));
+                $(".aim").text(aim - 1  + "˚");
+            }
+        };
+        aimWheel = function(event) {
+            var aim = Number($(".aim").text().substring(0, $(".aim").text().length - 1));
+            if ((event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) && (aim + 1) <= 360) {
+                $(".aim").text(aim + 1  + "˚");
+            }
+            else if ((aim + 1) >= 2) {
+                $(".aim").text(aim - 1  + "˚");
+            }
+            keyPressed = false;
+        };
+        $(window)
+            .on('mousewheel DOMMouseScroll', aimWheel)
+            .on("keydown", aimKeyPress)
+    }
+
+    function confirmAim() {
+        $(".aim").stop().animate({"font-size": 14}, 200);
+        $(window).off("click", confirmAim).off("mousewheel DOMMouseScroll", aimWheel).off("keydown", aimKeyPress);
+        $(".aim-button").on("click", setAim).stop().animate({"width": 35, backgroundColor: "#1f1f1f"}, 150, paramHover);
+    }
+
+    var powerKeyPress;
+    var powerWheel;
+
+    function setPower() {
+        $(".power").stop().animate({"font-size": 30}, 200);
+        $(".power-button").animate({"width": 100, backgroundColor: "#414141"}, 200).off();
+        setTimeout(function() {
+            $(window).on("click", confirmPower);
+        }, 50);
+        var keyPressed = false;
+        powerKeyPress = function(e) {
+            key = e.keyCode || e.charCode;
+            var power;
+            if ((key >= 48 || key >= 48) && (key <= 57 || key <= 48)) {
+                if (keyPressed == false) {
+                    power = 0;
+                    keyPressed = true;
+                } else {
+                    power = Number($(".power").text());
+                }
+                if (((power * 10) + (key - 48)) <= 100) {
+                    $(".power").text((power * 10) + (key - 48));
+                }
+            } else if (key == 38) {
+                power = Number($(".power").text());
+                $(".power").text(power + 1);
+            } else if (key == 40) {
+                power = Number($(".power").text());
+                $(".power").text(power - 1);
+            }
+        };
+        powerWheel = function(event) {
+            var power = Number($(".power").text());
+            if ((event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) && (power + 1) <= 100) {
+                $(".power").text(power + 1);
+            }
+            else if ((power + 1) >= 3) {
+                $(".power").text(power - 1);
+            }
+            keyPressed = false;
+        };
+        $(window)
+            .on('mousewheel DOMMouseScroll', powerWheel)
+            .on("keydown", powerKeyPress)
+    }
+
+    function confirmPower() {
+        $(".power").stop().animate({"font-size": 14}, 200);
+        $(window).off("click", confirmPower).off("mousewheel DOMMouseScroll", powerWheel).off("keydown", powerKeyPress);
+        $(".power-button").on("click", setPower).stop().animate({"width": 35, backgroundColor: "#1f1f1f"}, 150, paramHover);
+    }
+
+    $(".aim-button").on("click", setAim);
+    $(".power-button").on("click", setPower);
+    paramHover();
+    modeChange(tearTitle, buildGame);
 });
